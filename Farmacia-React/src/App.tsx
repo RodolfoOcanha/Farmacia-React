fd2b4710bcd14cd4b8bb10eb80aa866320dcaa76
RodolfoOcanha <rodolfo.ocanha@hotmail.com>
2024-04-22 12:57:53 -0300

    Correção APP tsx

--- Farmacia-React/src/App.tsx
+++ Farmacia-React/src/App.tsx
@@ -8,7 +8,9 @@ import Login from './paginas/login/Login';
 import Cadastro from './paginas/cadastro/Cadastro';
 import Home from './paginas/home/Home';
 import { AuthProvider } from './contexts/AuthContext';
-import ListaTemas from './components/categorias/listaCategorias/ListaCategorias';
+import ListaCategorias from './components/categorias/listaCategorias/ListaCategorias';
+import FormularioCategoria from './components/categorias/formularioCategoria/FormularioCategoria';
+import DeletarCategoria from './components/categorias/deletarCategoria/DeletarCategoria';
 
 
 function App() {
@@ -23,7 +25,10 @@ function App() {
               <Route path="/login" element={<Login />} />
               <Route path="/cadastro" element={<Cadastro />} />
               <Route path="/home" element={<Home />} />
-              <Route path="/temas" element={<ListaCategorias />} />
+              <Route path="/categorias" element={<ListaCategorias />} />
+              <Route path="/cadastroCategoria" element={<FormularioCategoria/>} />
+              <Route path="/editarCategoria/:id" element={<FormularioCategoria />} />
+              <Route path="/deletarCategoria/:id" element={<DeletarCategoria />} />
             </Routes>
           </div>
           <Footer />
