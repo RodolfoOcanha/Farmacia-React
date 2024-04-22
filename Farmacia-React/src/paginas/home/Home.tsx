8fb63eba2d0e3b144518089d983dc6aea726d392
RodolfoOcanha <rodolfo.ocanha@hotmail.com>
2024-04-22 12:48:35 -0300

    Crud

--- Farmacia-React/src/paginas/home/Home.tsx
+++ Farmacia-React/src/paginas/home/Home.tsx
@@ -2,19 +2,18 @@ import React from 'react';
 import homeLogo from '../../assets/home.png'
 import './Home.css';
 
-
 function Home() {
     return (
         <>
         <div className="bg-indigo-900 flex justify-center">
           <div className='container grid grid-cols-2 text-white'>
             <div className="flex flex-col gap-4 items-center justify-center py-4">
-              <h2 className='text-5xl font-bold'>Bem vindo a Farmácia da Gen</h2>
-              <p className='text-xl'>Encontre aqui os produtos que você procura!</p>
+              <h2 className='text-5xl font-bold'>Seja bem vindo a Farmácia Gen!</h2>
+              <p className='text-xl'>Encontre aqui os produtos que você procura</p>
   
               <div className="flex justify-around gap-4">
               
-                <button className='rounded bg-white text-blue-800 py-2 px-4'>Ver produtos</button>
+                <button className='rounded bg-white text-blue-800 py-2 px-4'>Ver postagens</button>
               </div>
             </div>
   
@@ -24,7 +23,7 @@ function Home() {
             </div>
           </div>
         </div>
-      
+        
       </>
     );
 }
